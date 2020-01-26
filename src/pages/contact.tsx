986ae3c9ad6584a6b39ac764e872551559f65180
Kite <ixkaito@gmail.com>
2020-01-26 11:44:19 +0900

    Initial commit

---
+++ src/pages/contact.tsx
@@ -0,0 +1,60 @@
+import Header from '../components/header'
+import ExtLink from '../components/ext-link'
+
+import sharedStyles from '../styles/shared.module.css'
+import contactStyles from '../styles/contact.module.css'
+
+import GitHub from '../components/svgs/github'
+import Twitter from '../components/svgs/twitter'
+import Envelope from '../components/svgs/envelope'
+import LinkedIn from '../components/svgs/linkedin'
+
+const contacts = [
+  {
+    Comp: Twitter,
+    alt: 'twitter icon',
+    link: 'https://twitter.com/ixkaito',
+  },
+  {
+    Comp: GitHub,
+    alt: 'github icon',
+    link: 'https://github.com/ixkaito',
+  },
+  // {
+  //   Comp: LinkedIn,
+  //   alt: 'linkedin icon',
+  //   link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+  // },
+  // {
+  //   Comp: Envelope,
+  //   alt: 'envelope icon',
+  //   link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+  // },
+]
+
+export default () => (
+  <>
+    <Header titlePre="Contact" />
+    <div className={sharedStyles.layout}>
+      <div className={contactStyles.avatar}>
+        <img src="/avatar.png" alt="avatar with letters JJ" height={60} />
+      </div>
+
+      <h1 style={{ marginTop: 0 }}>Contact</h1>
+
+      <div className={contactStyles.name}>
+        Kite - CEO @ <ExtLink href="https://kiteretz.com">KITERETZ</ExtLink>
+      </div>
+
+      <div className={contactStyles.links}>
+        {contacts.map(({ Comp, link, alt }) => {
+          return (
+            <ExtLink key={link} href={link} aria-label={alt}>
+              <Comp height={32} />
+            </ExtLink>
+          )
+        })}
+      </div>
+    </div>
+  </>
+)
